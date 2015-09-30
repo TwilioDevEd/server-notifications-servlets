9cdec38262b70975ad2bbe84c3f63d18c85430b7
Agustin Camino <agustin.camino@gmail.com>
2015-09-30 17:16:37 -0500

    Add `Administrator` model

---
+++ src/main/java/com/twilio/notifications/models/Administrator.java
@@ -0,0 +1,22 @@
+package com.twilio.notifications.models;
+
+public class Administrator {
+    private String name;
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
